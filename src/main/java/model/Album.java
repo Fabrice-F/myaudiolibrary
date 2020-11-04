20683a2851d70c3a44a2bd652bb717a8e6bd31b4
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-11-04 15:57:32 +0100

    Modification attribut et ajout des relation bi directionnel dans les classes

--- src/main/java/model/Album.java
+++ src/main/java/model/Album.java
@@ -12,10 +12,38 @@ public abstract class Album {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+
+    @ManyToOne
+    @JoinColumn(name = "ArtistId")
+    private Artist artist;
+
+
     private String title;
 
 
-    @OneToOne
-    @JoinColumn(name = "ArtistId",nullable = false)
-    private Artist ArtistId;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
 }
--- src/main/java/model/Artist.java
+++ src/main/java/model/Artist.java
@@ -1,6 +1,7 @@
 package model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 
 @Entity
@@ -12,8 +13,20 @@ public  abstract class Artist {
     @Column(name = "ArtistId")
     private Integer id;
 
+    @OneToMany(mappedBy = "artist")
+    private Set<Album> albums;
+
+
     private String Name;
 
+
+    public Set<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(Set<Album> albums) {
+        this.albums = albums;
+    }
 
     public String getName() {
         return Name;
