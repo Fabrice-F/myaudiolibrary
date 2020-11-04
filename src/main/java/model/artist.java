b0547522c708e772f08bee0a5ee9cf6483d09349
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-11-04 15:33:39 +0100

    Ajout du package modèle et des modlèes (artist et album)

--- src/main/java/model/artist.java
+++ src/main/java/model/artist.java
@@ -1,4 +1,25 @@
 package model;
 
-public class artist {
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "artist")
+public  abstract class artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ArtistId")
+    private Integer id;
+
+    private String Name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
