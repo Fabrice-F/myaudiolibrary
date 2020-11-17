4e8211bb3e6f80d61edb0398d8861031e6f25eb2
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-11-17 17:09:06 +0100

    Ajout des repository

---
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.myaudiolibrary.web.repository;
+
+import com.myaudiolibrary.web.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist,Integer> {
+}
