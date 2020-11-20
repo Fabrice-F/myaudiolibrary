7ec5041ce5f6faaf258a53c121113433e0f07d19
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-11-20 13:34:29 +0100

    gestions des erreurs exercice 2

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -30,6 +31,10 @@ public class ArtistController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE,value = "/{id}")
     public Optional<Artist> searchArtistById(@PathVariable(value = "id") Integer id){
+
+        Optional<Artist> optionalArtist = artistRepository.findById(id);
+        if (optionalArtist.isEmpty())
+            throw new EntityNotFoundException("L'artiste avec l'id " + id + " n'as pas été trouvé ");
         return artistRepository.findById(id);
     }
 
@@ -42,6 +47,13 @@ public class ArtistController {
             @RequestParam(value = "sortDirection" ) Sort.Direction sortDirection
     )
     {
+        //TODO: SI NAME EXISTE OU PAS VIDE
+        if (size<10|| size>50)
+            throw new IllegalArgumentException("L'argument size n'est pas situé entre les bornes 10 et 50");
+        // TODO: TRAITEMENT PAGE <1
+        // TODO: TRAITEMENT SI INTEGER RECOIT UN UN AUTRE TYPE
+        // TODO: TRAITEMENT SI SORT DIRECTION INVALIDE
+        // TODO: TRAITEMENT SI SORT PROPERTY NON RENSEIGNER OU INCORRECT ( valeur par défaut)
         PageRequest pageRequest = PageRequest.of(page,size,sortDirection,sortProperty);
         return artistRepository.findAllByNameIgnoreCaseContaining(name,pageRequest);
     }
@@ -53,6 +65,14 @@ public class ArtistController {
             @RequestParam(value = "sortProperty") String value,
             @RequestParam(value = "sortDirection" ) Sort.Direction sortDirection
     ){
+        if (size<10|| size>50)
+            throw new IllegalArgumentException("L'argument size n'est pas situé entre les bornes 10 et 50");
+        // TODO: TRAITEMENT PAGE <1
+        // TODO: TRAITEMENT SI INTEGER RECOIT UN UN AUTRE TYPE
+        // TODO: TRAITEMENT SI SORT DIRECTION INVALIDE
+        // TODO: TRAITEMENT SI SORT PROPERTY NON RENSEIGNER OU INCORRECT ( valeur par défaut)
+
+
         PageRequest pageRequest = PageRequest.of(page,size,sortDirection,value);
         return artistRepository.findAll(pageRequest);
     }
@@ -61,6 +81,9 @@ public class ArtistController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     public Artist createArtist(@RequestBody Artist artist)
     {
+        // TODO: TRAITEMENT SI ARTISTE N'EST PAS NULL
+        // TODO: TRAITEMENT SI ARTISTE EXISTE DEJA
+        // TODO: TRAITEMENT SI MANQUE DES INFORMATIONS A ARTISTE (voir required décorateur)
         Artist result =  artistRepository.save(artist);
         return result;
     }
@@ -68,9 +91,11 @@ public class ArtistController {
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE,value = "/{id}")
     public Artist updateArtist(@PathVariable Integer id,@RequestBody Artist artist)
     {
-        System.out.println(id);
-        System.out.println(artist.getName());
 
+        // TODO: TRAITEMENT SI ID EST SUPERIEUR A 0 ET SI BONNE CONVERSION
+        // TODO: TRAITEMENT SI ARTIST EXISTE
+        // TODO: TRAITEMENT SI ARTIST N'EST PAS VIDE
+        // TODO: TRAITEMENT SI MANQUE DES INFORMATIONS A ARTISTE (voir required décorateur)
         Artist result =  artistRepository.save(artist);
         return result;
     }
@@ -79,7 +104,8 @@ public class ArtistController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteArtiste(@PathVariable("id") Integer id)
     {
-
+        // TODO: TRAITEMENT SI ID EST SUPERIEUR A 0 ET SI BONNE CONVERSION
+        // TODO: TRAITEMENT SI ARTIST EXISTE
         Artist artist = artistRepository.findById(id).get();
         artistRepository.delete(artist);
     }
